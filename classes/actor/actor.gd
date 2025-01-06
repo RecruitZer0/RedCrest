57871bfeed0f609235bb1d2b8ad20bd041b2239e
Recruit_Zer0 <59139794+RecruitZer0@users.noreply.github.com>
2025-01-05 21:24:27 -0300

    Added NPCs and ActorTags
    The first type of NPC added is FollowNPC, it follows. yay.
    I'm tired of writing commit descriptions, that's it.
    Oh yeah I added ActorTags too, that's important.

--- classes/actor/actor.gd
+++ classes/actor/actor.gd
@@ -3,6 +3,11 @@ class_name Actor extends CharacterBody3D
 ##
 ## Has code for gravity, movement and shadow. All can be disabled.
 
+enum Tags {
+	PLAYER,
+	SOLDIER,
+	NEUTRAL,
+}
 
 @onready var sprite: Sprite3D = $Sprite
 @onready var shadow: RayCast3D = $Shadow
@@ -10,21 +15,29 @@ class_name Actor extends CharacterBody3D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var damage_receiver: DamageReceiver = $DamageReceiver
+@onready var visible_on_screen: VisibleOnScreenNotifier3D = $VisibleOnScreen
 
 
-@export_category("Actor Movement")
+@export_group("Movement")
 @export var max_speed := 8.0
 @export var acceleration := 40.0
 @export var gravity := 30.0
 @export var speed_multiplier := 1.0
 @export var should_move := true
+@export var can_fly := false
 var movement_dir: Vector3
 var velocity_add: Vector3
 
-@export_category("Visuals")
+@export_group("Visuals")
 @export var shadow_radius := 1.0
 @export var should_flip_offset := true ## If true, when the [member sprite]'s [param flip_h] or [param flip_h] are true, its [member SpriteBase3D.offset] will be flipped as well.
 
+@export_group("")
+@export var tags: Array[Tags] = [] ## An Array of [enum Tags] that represents what Tags this Actor is included in.
+
+## Returns whether this Actor is on screen. It uses [method VisibleOnScreenNotifier3D.is_on_screen]
+func is_on_screen() -> bool:
+	return visible_on_screen.is_on_screen()
 
 
 func _ready() -> void:
@@ -36,12 +49,14 @@ func _process(_delta: float) -> void:
 		animation_tree.mixer_applied.connect(_flip_offset)
 	if not should_flip_offset and animation_tree.mixer_applied.is_connected(_flip_offset):
 		animation_tree.mixer_applied.disconnect(_flip_offset)
+	visible_on_screen.layers = sprite.layers
 
 
 func _physics_process(delta: float) -> void:
-	if is_on_floor():
+	if is_on_floor() or can_fly:
 		velocity = velocity.move_toward(movement_dir * max_speed * speed_multiplier, acceleration * delta * speed_multiplier)
-	velocity.y -= gravity * delta
+	else:
+		velocity.y -= gravity * delta
 	velocity += velocity_add
 	if should_move:
 		move_and_slide()
@@ -54,12 +69,11 @@ func _handle_shadow() -> void:
 	shadow_sprite.visible = shadow.is_colliding()
 	if shadow.is_colliding():
 		var pos := shadow.get_collision_point() - global_position
-		shadow_sprite.position = pos + (Vector3.UP * 0.5) + (Vector3.BACK)
+		shadow_sprite.position = pos + (Vector3.UP * 0.1)
 		shadow_sprite.scale = Vector3.ONE * remap(pos.y, 0, -15, 1, 0) * shadow_radius
 		shadow_sprite.modulate.a = remap(pos.y, 0, -15, 1, 0)
 
 func _flip_offset() -> void:
-	#await get_tree().process_frame
 	if sprite.flip_h:
 		sprite.offset.x *= -1
 	if sprite.flip_v:
---
+++ classes/actor/npc/follow_npc.gd
@@ -0,0 +1,96 @@
+class_name FollowNPC extends Actor
+## The most basic and versatile type of NPC.
+##
+## It uses [NavigationAgent3D] (defined in [member nav]) to follow and attack any [Actor] by their tags.
+
+@onready var nav: NavigationAgent3D = $NavigationAgent ## The NavigationAgent3D used to move.
+@onready var ray_wall: RayCast3D = $RayWall ## The [Raycast3D] used to detect walls in front of it to jump.
+@onready var ray_ground: RayCast3D = $RayGround ## The [Raycast3D] used to detect holes in front of it to jump.
+
+@export_group("Follow")
+@export var follow_targets: Array[Actor.Tags] = [] ## An Array of [enum Actor.Tags] that the NPC should follow and target its attacks.
+@export var minimum_distance: float = 2 ## A minimum distance the NPC will try to maintain when following the target.
+@export var automatically_target := true ## If true, the NPC will be constantly trying to target whichever valid target is the closest when it doesn't have one already (see [method is_valid_target]). Note that the NPC will only do that when it is on screen.
+@export var switch_to_attacker := true ## If true, the NPC will switch its target to whoever last attacked them.
+@export_subgroup("Align", "align_")
+@export_flags("X", "Y", "Z") var align_axis := 1 ## When set, the NPC will try to get it's respective axis as close as possible to the target's axis.
+@export var align_margin: float = 0.4 ## To avoid the NPC trying to make it's [member align_axis] [i]exactly[/i] equal to the target, this margin provides a threshold that counts as equal value.
+@export_subgroup("Jumping", "jump_")
+@export var jump_force := 20.0 ## Force of the jump.
+@export var jump_wall_distance := 3.0 ## The distance the [member ray_wall] will check forward for collisions.
+@export var jump_ground_distance := 2.0 ## The distance the [member ray_ground] will check forward for collisions.
+@export var jump_ground_depth := 10.0 ## The distance that the [member ray_ground] will check down for collisions.
+
+var targeting: Actor
+
+
+func _physics_process(delta: float) -> void:
+	if targeting:
+		_handle_pathing()
+	elif is_on_screen() and automatically_target:
+		targeting = find_nearest_target()
+	
+	super(delta)
+
+
+func find_nearest_target() -> Actor:
+	var actors := get_tree().get_nodes_in_group("Actor")
+	actors = actors.filter(is_valid_target)
+	if actors.is_empty():
+		return null
+	actors.sort_custom(func(a, b): return global_position.distance_squared_to(a.global_position) < global_position.distance_squared_to(b.global_position))
+	return actors.front()
+
+## Returns if [param actor] is a valid and safe target for this NPC. I.e., the Actor is on screen and it has one of the tags in [member follow_targets].
+## Note that even though the NPC only searches for targets when itself is on screen, this action doesn't consider that. (see [member automatically_target])
+func is_valid_target(actor: Actor) -> bool:
+	if not actor.is_on_screen():
+		return false
+	
+	for my_tag in follow_targets:
+		if actor.tags.has(my_tag):
+			return true
+	return false
+
+## Returns if both the [NavigationServer] has started completely and [NavigationAgent3D] has not reached it's destination.
+func is_ai_ready() -> bool:
+	if NavigationServer3D.map_get_iteration_id(nav.get_navigation_map()) == 0:
+		return false
+	if nav.is_navigation_finished():
+		return false
+	return true
+
+func _switch_to_attacker(damage: Damage) -> void:
+	if not switch_to_attacker: return
+	if damage.inflictor.owner is Actor:
+		targeting = damage.inflictor.owner
+
+func _handle_pathing() -> void:
+	if align_axis:
+		var align_byte := String.num_int64(align_axis, 2).pad_zeros(3)
+		var align_vector := Vector3.ZERO
+		for i in range(3):
+			align_vector[i] = align_byte[~i].to_int() * minimum_distance
+		align_vector *= (global_position - targeting.global_position).sign()
+		nav.target_position = targeting.global_position + align_vector
+		nav.target_desired_distance = align_margin
+	else:
+		nav.target_position = targeting.global_position
+		nav.target_desired_distance = minimum_distance
+	
+	if is_ai_ready():
+		var relative_path_position := nav.get_next_path_position()
+		relative_path_position.y = global_position.y
+		
+		nav.path_height_offset = shadow_sprite.position.y
+		movement_dir = global_position.direction_to(relative_path_position)
+		
+		ray_wall.target_position = global_position.direction_to(relative_path_position) * jump_wall_distance
+		ray_ground.position = global_position.direction_to(relative_path_position) * jump_ground_distance + Vector3.UP
+		ray_ground.target_position = Vector3.DOWN * jump_ground_depth + Vector3.DOWN
+		
+		if (ray_wall.is_colliding() or not ray_ground.is_colliding()) and is_on_floor():
+			if is_equal_approx(velocity.x, 0) or is_equal_approx(velocity.z, 0):
+				movement_dir *= -1
+			else:
+				velocity_add.y += jump_force
